863785d54272dce85879aba501ea857aec0a00d1
Oleksandr Barabash <barabashsan4ik@gmail.com>
2022-11-21 19:24:06 +0200

    new1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./components/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/hw-react-5-movies">
+    <BrowserRouter basename="/react-movie">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
