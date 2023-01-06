e4811add59efb0ea648df56945470b839501b038
OleksandrB93 <barabashsan4ik@gmail.com>
2023-01-06 13:04:10 +0200

    add shadow

--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -35,8 +35,11 @@ export const FormContainer = styled.form`
     rgba(47, 48, 58, 0.4),
     rgba(47, 48, 58, 0.4)
   );
-  box-shadow: rgba(192, 192, 192, 0.5) 0px 14px 28px,
-    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
 `;
 
 export const TitleForm = styled.p`
